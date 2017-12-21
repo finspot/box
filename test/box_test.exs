da81a35f58dae14ef7d24c6482795093fb8700ae
david.ruyer+github@gmail.com <david.ruyer@gmail.com>
2017-12-21 11:27:43 +0100

    [Feat] Change file renaming

--- test/box_test.exs
+++ test/box_test.exs
@@ -13,8 +13,8 @@ defmodule BoxTest do
   test "happy path with renaming" do
     Box.Mock.mock(fn
       {:files, "folder_id"} -> {:ok, []}
-      {:upload, "folder_id", "foo - page 1.pdf", "/tmp/foo.pdf"} -> {:ok, "box_id_1"}
-      {:upload, "folder_id", "foo - page 2.pdf", "/tmp/foo.pdf"} -> {:ok, "box_id_2"}
+      {:upload, "folder_id", "foo.pdf", "/tmp/foo.pdf"} -> {:ok, "box_id_1"}
+      {:upload, "folder_id", "foo (1).pdf", "/tmp/foo.pdf"} -> {:ok, "box_id_2"}
     end)
 
     assert {:ok, "box_id_1"} = Box.upload("folder_id", "foo.pdf", "/tmp/foo.pdf")
@@ -31,7 +31,7 @@ defmodule BoxTest do
   test "box is down" do
     Box.Mock.mock(fn
       {:files, "down_folder_id"} -> {:ok, []}
-      {:upload, "down_folder_id", "foo - page 1.pdf", "/tmp/foo.pdf"} -> {:error, :box_error}
+      {:upload, "down_folder_id", "foo.pdf", "/tmp/foo.pdf"} -> {:error, :box_error}
     end)
 
     assert {:error, :box_error} = Box.upload("down_folder_id", "foo.pdf", "/tmp/foo.pdf")
